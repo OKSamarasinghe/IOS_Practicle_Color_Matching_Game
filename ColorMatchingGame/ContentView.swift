0c1464e50f530c03e857d62ea04930aaf0228ad7
OKSamarasinghe <oshadakaveensamarasinghe@gmail.com>
2026-01-12 15:33:57 +0530

    first commit

--- ColorMatchingGame/ContentView.swift
+++ ColorMatchingGame/ContentView.swift
@@ -1,21 +1,54 @@
-//
-//  ContentView.swift
-//  ColorMatchingGame
-//
-//  Created by COBSCCOMP242P069 on 2026-01-12.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
+    let colors: [Color] = [.red, .green, .blue]
+    let colorNames = ["Red", "Green", "Blue"]
+
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var resultMessage = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+
+            Text("Color Matching Game")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Match the color:")
+                .font(.title2)
+
+            Text(colorNames[correctAnswer])
+                .font(.title)
+                .foregroundColor(colors[correctAnswer])
+
+            ForEach(0..<3) { index in
+                Button {
+                    checkAnswer(index)
+                } label: {
+                    Rectangle()
+                        .fill(colors[index])
+                        .frame(height: 60)
+                        .cornerRadius(12)
+                }
+            }
+
+            Text(resultMessage)
+                .font(.title2)
+                .fontWeight(.semibold)
+
         }
         .padding()
+    }
+
+    func checkAnswer(_ selectedIndex: Int) {
+        if selectedIndex == correctAnswer {
+            resultMessage = "✅ Correct!"
+        } else {
+            resultMessage = "❌ Try Again"
+        }
+
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
