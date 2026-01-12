4a7fd12b4f8be5807a32f108556e98836e8c5db1
OKSamarasinghe <oshadakaveensamarasinghe@gmail.com>
2026-01-12 14:38:20 +0530

    Initial Commit

---
+++ ColorMatchingGame/ColorMatchingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMatchingGameApp.swift
+//  ColorMatchingGame
+//
+//  Created by COBSCCOMP242P069 on 2026-01-12.
+//
+
+import SwiftUI
+
+@main
+struct ColorMatchingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
